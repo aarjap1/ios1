8757c478da4deac7027d3ae496beefd1866172a8
Aarjap <43009479+aarjap1@users.noreply.github.com>
2019-01-16 09:10:59 -0600

    Add files via upload

---
+++ Swift Programming Exercises.playground/Contents.swift
@@ -0,0 +1,52 @@
+// Swift Programming Exercises
+// Answer each question after the comment, then zip this and upload it
+
+// You can do this using the computers in CH, Owens Library, checking out a MBP from Owens Library (3 days max) or go to repl.it, and create a Swift REPL
+
+import UIKit
+
+
+
+// 0. Declare a constant pi and assign it the value 3.14159. Use type annotation
+
+// 1. Declare a variable, isCold, and assign the value true. Use type annotation
+
+// 2. Declare a variable, fastFood, with the value "Jimmy Johns". Use type inference.
+
+// 2.1 Declare a variable, age, with the value 18. Then print out the statement "You are xx years old", replacing xx with age. Use type interpolation to do so.
+
+// 3. Declare an empty array of Strings, movies. Use type annotation.
+var movies:[String] = [ ]
+
+// 4. Repeat 3, but this time using type inference. What error do you get? [After determining the error, comment out the statement so the rest of your playground will run.]
+
+
+// 5. Add a new movie, "Rogue One", to movies
+
+// 6. Add 2 new movies, "Zootopia" and "Wonder Woman" (in one statement)
+
+// 7. Write code to print out all the elements of movies. Do so using an explicit index and [ ] to access the elements.
+
+// 8. Repeat 6 using a for-in loop
+
+// 9. Write code to print out the numbers 0, 3, 6, 9, ... 21. Use stride(from:to:by:)
+
+// 10. Write code to print out the numbers 100, 90, 80, ... 0. Use stride(from:through:by:)
+
+// 11. Declare an array of Ints, data, with 10 unique values.
+
+// 12. Write the code to find the smallest value in data and then print it out.
+
+// 13. Write the code to find the sum of all the elements in data
+
+// 14. Write a switch statement to analyze roll (an Int between 2-12, representing the roll of 2 dice). If it is 7 or 11, print "Win!". If it is a 2, 3 or 12, print "Crap Out". If it is  6, print "Perfection". Otherwise, print "Try again"
+
+    var roll = Int.random(in: 1...6) + Int.random(in:1...6) // generates 2 random Ints, each between 1 and 6 inclusive
+
+let pi:Double = 3.14159
+
+var isCold:Bool = true
+
+var
+
+
